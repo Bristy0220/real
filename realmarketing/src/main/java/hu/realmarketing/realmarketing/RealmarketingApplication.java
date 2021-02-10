71abf9d647d360d602aa1b3c1610a95077fce1c0
Bristy0220 <krisztian.0220@gmail.com>
2021-02-10 13:17:27 +0000

    First Commit

---
+++ realmarketing/src/main/java/hu/realmarketing/realmarketing/RealmarketingApplication.java
@@ -0,0 +1,13 @@
+package hu.realmarketing.realmarketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealmarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealmarketingApplication.class, args);
+	}
+
+}
---
+++ realmarketing/src/test/java/hu/realmarketing/realmarketing/RealmarketingApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.realmarketing.realmarketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealmarketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
